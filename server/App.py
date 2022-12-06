18d37455d70b7d7544564b5924258351f048f768
Lee Sang Hoon <shpusan001@gmail.com>
2022-12-06 13:00:03 +0900

    Fix: 서버에 cors 옵션 추가, 프론트에 proxy 추가

--- server/App.py
+++ server/App.py
@@ -1,10 +1,11 @@
+import src.router.ApkRouter as ApkRouter
+import src.router.ParsingRouter as ParsingRouter
+import src.router.DefaultRouter as DefaultRouter
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import sys
 sys.path.append('.')
 
-import src.router.DefaultRouter as DefaultRouter
-import src.router.ParsingRouter as ParsingRouter
-import src.router.ApkRouter as ApkRouter
 
 # python-multipart, construct 2.5.3, fastapi
 
@@ -13,3 +14,13 @@ app = FastAPI()
 app.include_router(DefaultRouter.router)
 app.include_router(ParsingRouter.router)
 app.include_router(ApkRouter.router)
+
+origins = ["http://localhost:3000",]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
