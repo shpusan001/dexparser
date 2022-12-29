da4f05422e18253c20169e63eb04299b4140d78a
Lee Sang Hoon <shpusan001@gmail.com>
2022-12-30 08:12:30 +0900

    Feat: CDexparserTest ㅊㅜ가

---
+++ server/src_test/util/dex_parser/CDexParserTest.py
@@ -0,0 +1,31 @@
+import pprint
+from src.util.HashDigester import HashDigester
+from src.util.dex_parser.DictDexParser import DictDexParser
+from src_test.test_init.DefaultTestInit import TestInit
+import unittest
+import sys
+import os
+sys.path.append('.')
+
+
+class CDexParserTest(TestInit):
+
+    def test_getParsedData(self):
+        dictDexParser = DictDexParser()
+        print(os.getcwd())
+        dictDexParser.setFileFullPath("./src_test/res/classes.dex")
+
+        tempFilePath = "./src_test/res/temp1.txt"
+
+        with open(tempFilePath, mode="w") as fp:
+            pprint.pprint(dictDexParser.getParsedData(), stream=fp)
+
+        sha1 = HashDigester().sha1ForLageFile(tempFilePath)
+
+        self.assertEqual(sha1, "178d7082f28638cd62f904976b770ba269b1ff61")
+
+        os.remove(tempFilePath)
+
+
+if __name__ == '__main__':
+    unittest.main()
